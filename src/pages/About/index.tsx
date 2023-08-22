f7486d43489e3af11005988823e2a3a1c37c872a
Yagnesh Modh <contact@thespecialcharacter.com>
2023-08-22 15:39:02 +0530

    clean

--- src/pages/About/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const About = (props: Props) => {
-  return <div>About page</div>;
-};
-
-export default About;
--- src/pages/NotFound/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const NotFound = (props: Props) => {
-  return <div>Page Not Found</div>;
-};
-
-export default NotFound;
